c2abd16f8fb227c56cdf01d6d6ca921ec49173dc
Elessa <Elessa@no.com>
2024-03-06 17:02:41 +0100

    0

--- models/train_model.py
+++ models/train_model.py
@@ -3,13 +3,15 @@ import torch
 from model_transformer import TransformerModel  # 假设你的模型定义在这个文件中
 from data_loder import get_data_loader
 
-def train_model(train_data_loader, validation_data_loader, model, epochs=1):
+def train_model(device, train_data_loader, validation_data_loader, model, epochs=1):
     optimizer = torch.optim.Adam(model.parameters())
     loss_fn = torch.nn.CrossEntropyLoss()
 
     for epoch in range(epochs):
         model.train()
         for input_ids, target_ids in train_data_loader:
+            input_ids = input_ids.to(device)
+            target_ids = target_ids.to(device)
             optimizer.zero_grad()
             output = model(input_ids)
             output = output.permute(0, 2, 1)
@@ -22,8 +24,12 @@ def train_model(train_data_loader, validation_data_loader, model, epochs=1):
         with torch.no_grad():
             total_loss = 0
             for input_ids, target_ids in validation_data_loader:
+                input_ids = input_ids.to(device)
+                target_ids = target_ids.to(device)
                 output = model(input_ids)
+                output = output.permute(0, 2, 1)
                 loss = loss_fn(output, target_ids)
+                output = output.permute(0, 2, 1)
                 total_loss += loss.item()
             print(f'Epoch {epoch}, Validation Loss: {total_loss / len(validation_data_loader)}')
 
@@ -34,6 +40,6 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # 初始化模型
 model = TransformerModel(vocab_size=30000, n_layer=3, n_head=4, n_emb=16, context_length=256, pad_token_id=0)  # 假设你的模型构造函数不需要任何参数
-
+model.to(device)
 # 训练模型
-train_model(train_data_loader, validation_data_loader, model)
+train_model(device ,train_data_loader, validation_data_loader, model)
